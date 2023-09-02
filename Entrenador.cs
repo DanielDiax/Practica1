08fecf09ad0f569e8fb3ffe4ee2b7915599c4dde
Daniel Diaz Valencia <jose.diaz773@pascualbravo.edu.co>
2023-09-02 15:43:30 -0500

    Usos eliminados

--- Entrenador.cs
+++ Entrenador.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Practica1
 {
-    
+
     class Entrenador: MiembroSeleccionFutbol
     {
         string federacion;
--- MiembroSeleccionFutbol.cs
+++ MiembroSeleccionFutbol.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Practica1
+﻿namespace Practica1
 {
     public abstract class MiembroSeleccionFutbol
     {
